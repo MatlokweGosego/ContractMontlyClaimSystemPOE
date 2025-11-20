f28253cee0ffdf2c3e4a1a838791dedfa46a3dbe
MatlokweGosego <166375215+MatlokweGosego@users.noreply.github.com>
2025-11-20 23:48:14 +0200

    Added Automated Reporting

---
+++ Services/ReportService.cs
@@ -0,0 +1,15 @@
+﻿namespace ContractMontlyClaimSystemPOE.Services
+{
+    public class ReportService : IReportService
+    {
+        public byte[] GenerateInvoicePdf(Claim claim)
+        {
+            // Use libraries like QuestPDF or iTextSharp
+            return GeneratePdfInvoice(claim);
+        }
+
+        public List<Claim> GetMonthlyReport(DateTime month)
+        {
+            return GetClaimsForMonth(month);
+        }
+    }
