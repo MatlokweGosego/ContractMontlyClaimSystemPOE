c4a3e7719e75f7f53cd3bd388b6c09a2357f5cc1
MatlokweGosego <166375215+MatlokweGosego@users.noreply.github.com>
2025-11-20 23:45:41 +0200

    Created HR Controller

---
+++ Controllers/HRController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMontlyClaimSystemPOE.Controllers
+{
+    public class HRController : Controller
+    {
+        public async Task<IActionResult> Dashboard()
+        {
+            // HR dashboard with statistics
+            return View();
+        }
+
+        public async Task<IActionResult> ApprovedClaims()
+        {
+            var claims = await GetApprovedClaimsForPayment();
+            return View(claims);
+        }
+
+        public async Task<IActionResult> GenerateInvoice(int claimId)
+        {
+            var invoice = await GenerateInvoiceData(claimId);
+            return View(invoice);
+        }
+
+        public async Task<IActionResult> ManageLecturers()
+        {
+            var lecturers = await GetAllLecturers();
+            return View(lecturers);
+        }
+    }
+}
